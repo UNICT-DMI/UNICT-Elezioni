b03fc7b78ed354a22ccc3f57fe87046019c705c5
v0lp3 <andreamaujeri@gmail.com>
2020-09-11 22:02:00 +0200

    feat(review) added readme and reviewed code

---
+++ jsonParser/README.md
@@ -0,0 +1,10 @@
+# Json Parser
+
+### `ts-node parser.ts $fileName [0|other]`
+
+`$fileName` is the name of the file from which you want extract data. </br>
+
+`[0|other]` indicates the type of data to parse.
+
+You shall choose `0` if you want extract data from departments pdf. </br>
+Otherwise you can omit, `other` is default config. 
--- jsonParser/parser.ts
+++ jsonParser/parser.ts
@@ -1,7 +1,6 @@
 const pdf2table = require('pdf2table');
 import * as fs from 'fs';
 
-
 const enum schede {
     BIANCHE = 'Schede Bianche',
     NULLE = 'Schede Nulle',
@@ -29,7 +28,7 @@ const enum elettori {
 
 const enum query {
     DIPARTIMENTO = 'DIPARTIMENTO',
-    ORGANI = "IN SENO"
+    ORGANI = 'IN SENO'
 }
 
 class Parser {
@@ -38,7 +37,6 @@ class Parser {
     private fileName: string;
 
     constructor(fileName: string, dip) {
-
         this.fileName = fileName;
 
         this.info = {
@@ -47,20 +45,24 @@ class Parser {
             eletti: []
         }
 
-        if (dip === 0)
-            this.doc = new Dipartimento();
-        else
-            this.doc = new Organo();
-
-
+        switch (dip) {
+            case 0:
+                this.doc = new Dipartimento;
+                break;
+            default:
+                this.doc = new Organo();
+                break;
+        }
     }
 
     private write(): void {
         const data = JSON.stringify(this.info);
         fs.writeFile('json/' + this.fileName + '.json', data, (errW) => {
+
             if (errW) {
                 throw errW;
             }
+
             console.log('JSON data is saved.');
         });
     }
@@ -68,8 +70,9 @@ class Parser {
     public scrape(): void {
         fs.readFile('document/' + this.fileName + '.pdf', (errR, buffer) => {
 
-            if (errR)
+            if (errR) {
                 return console.log(errR);
+            }
 
             pdf2table.parse(buffer, (errP, data) => {
                 if (errP)
@@ -88,7 +91,6 @@ class Parser {
                         this.info['eletti'].push(eletto);
                     }
 
-
                     switch (el[0]) {
                         case schede.BIANCHE:
                         case schede.NULLE:
@@ -102,23 +104,15 @@ class Parser {
                             this.info['votanti'] = el[1];
                             break;
                     }
-                    if (el[0].includes(seggi.SCRUTINATI))
+
+                    if (el[0].includes(seggi.SCRUTINATI)) {
                         idxList++;
-
+                    }
                 });
-
-
-
-
-
                 this.write();
             });
         });
     }
-
-
-
-
 }
 
 interface Target {
@@ -127,13 +121,13 @@ interface Target {
     checkEletto(data: object): boolean;
 }
 
-
 class Dipartimento implements Target {
     public scrapeLists(info: object, data: object[]): void {
         for (let i = 0; i < data.length; i++) {
 
-            if (data[i][0].includes(query.DIPARTIMENTO))
+            if (data[i][0].includes(query.DIPARTIMENTO)) {
                 info['dipartimento'] = data[++i][0];
+            }
 
             if (data[i][0].includes(candidati.LISTE_DIP)) {
                 i = i + 2;
@@ -144,25 +138,26 @@ class Dipartimento implements Target {
             }
         }
     }
+
     public scrapeOther(info: object, data: object): void {
-        if (data[0] === seggi.DA_ASSEGNARE_DIP)
-            info["seggi_da_assegnare"] = data[1];
+        if (data[0] === seggi.DA_ASSEGNARE_DIP) {
+            info['seggi_da_assegnare'] = data[1];
+        }
     }
+
     public checkEletto(data: object): boolean {
-        if (data[2] === candidati.ELETTO_DIP)
-            return true;
-
-        return false;
+        return data[2] === candidati.ELETTO_DIP;
     }
-
 }
 
 class Organo implements Target {
+
     public scrapeLists(info: object, data: object[]): void {
         for (let i = 0; i < data.length; i++) {
 
-            if (data[i][0].includes(query.ORGANI))
+            if (data[i][0].includes(query.ORGANI)) {
                 info['organo'] = data[i][0];
+            }
 
             if (data[i][0].includes(candidati.LISTE_ORG)) {
                 while (!data[++i][0].includes(candidati.VOTI) && !data[i][0].includes(schede.BIANCHE))
@@ -170,21 +165,19 @@ class Organo implements Target {
             }
         }
     }
-    public scrapeOther(info: object, data: object): void {
-        if (data[1] === seggi.DA_ASSEGNARE_ORG)
-            info["seggi_da_assegnare"] = data[2];
-    }
-    public checkEletto(data: object): boolean {
-        if (data[2] === candidati.ELETTO_ORG)
-            return true;
 
-        return false;
+    public scrapeOther(info: object, data: object): void {
+        if (data[1] === seggi.DA_ASSEGNARE_ORG) {
+            info['seggi_da_assegnare'] = data[2];
+        }
+    }
+
+    public checkEletto(data: object): boolean {
+        return data[2] === candidati.ELETTO_ORG;
     }
 }
 
-
-let fileName = process.argv[2];
-let mode = parseInt(process.argv[3]);
+const fileName = process.argv[2];
+const mode = parseInt(process.argv[3]);
 const creator = new Parser(fileName, mode);
 creator.scrape();
-
