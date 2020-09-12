f2cee59ab9a32bf5ff65462108c524c4a8ae10b7
v0lp3 <andreamaujeri@gmail.com>
2020-09-12 15:06:03 +0200

    chore(style) minor changes

--- src/app/container/Container.tsx
+++ src/app/container/Container.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent } from 'react';
 import './Container.scss';
+import { faDemocrat } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Container: FunctionComponent = () => {
   return (
     <div className="container-body">
-      
-      <h1>Risultati elezioni</h1>
+      <h3><FontAwesomeIcon icon={faDemocrat} /> Risultati elezioni </h3>
       <ul>
-        <li><button>Dipartimento & CdL</button></li>
+        <li><button className="btn-grad">Dipartimento & CdL</button></li>
         <li><button>Organi superiori</button></li>
       </ul>
     </div>
