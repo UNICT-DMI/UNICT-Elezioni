cf9d9f9717c32aff923d95c5f7bde6cd44e6ffe6
v0lp3 <andreamaujeri@gmail.com>
2020-09-12 14:24:39 +0200

    chore(tab) added button

--- src/app/container/Container.tsx
+++ src/app/container/Container.tsx
@@ -4,14 +4,12 @@ import './Container.scss';
 const Container: FunctionComponent = () => {
   return (
     <div className="container-body">
-      <h1>Lorem ipsum</h1>
-      <h2>Lorem ipsum</h2>
-      <h3>Lorem ipsum</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet cursus consequat.
-      Nam nec nibh id mauris blandit vehicula. Pellentesque in augue eu massa pulvinar efficitur.
-      Aliquam elementum quam ornare, mollis nisi a, tempus erat. Vivamus faucibus efficitur ligula,
-      vel convallis risus dignissim sed. Proin tristique mattis nunc, ut ultricies est fringilla ac. 
-    </p>
+      
+      <h1>Risultati elezioni</h1>
+      <ul>
+        <li><button>Dipartimento & CdL</button></li>
+        <li><button>Organi superiori</button></li>
+      </ul>
     </div>
   );
 }
