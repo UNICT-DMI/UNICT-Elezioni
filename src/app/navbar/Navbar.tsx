9c780d5a1f009f21ad368af31d0861e636935c3a
v0lp3 <andreamaujeri@gmail.com>
2020-09-12 11:58:41 +0200

    style(navbar) removed bootstrap library

--- src/app/navbar/Navbar.tsx
+++ src/app/navbar/Navbar.tsx
@@ -6,15 +6,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar: FunctionComponent = () => {
   return (
     <div className="Navbar">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="#">UNICT-Elezioni</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <div className="navbar-nav mr-auto"></div>
-            <FontAwesomeIcon icon={faGithub} />
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a className="navbar-el" href="#" id="navbar-brand">UNICT-Elezioni</a>
+          <FontAwesomeIcon icon={faGithub} className="navbar-el" />
         </div>
       </nav>
     </div>
