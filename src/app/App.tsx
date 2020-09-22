04f0460923d4e35ee045ba76a892b35ba74aa01c
adrenoid <graziano27dg@gmail.com>
2020-09-22 21:36:51 +0200

    feat(data): Add blank, null, spoiled vote, and minor improvement

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,39 +9,44 @@ import './App.scss';
 import Menu from './navbar/Navbar';
 import Results from './results/Results';
 import { dipartimenti } from '../data/dipartimenti';
+import Card from 'react-bootstrap/Card';
 
 const App: FunctionComponent = () => {
   return (
     <div className="App">
       <HashRouter basename="/">
         <Menu />
-        <Switch>
-          <Route exact path="/senato">
-            <h2 className="mt-5">Senato</h2>
-            <br />
-            <Results anno="2018-2020" path="Senato" />
-            <Results anno="2016-2018" path="Senato" />
-          </Route>
-          <Route exact path="/cda">
-            <h2 className="mt-5">Consiglio di Amministrazione</h2>
-            <br />
-            <Results anno="2018-2020" path="Consiglio_di_amministrazione" />
-            <Results anno="2016-2018" path="Consiglio_di_amministrazione" />
-          </Route>
-          <Route exact path="/dipartimenti">
-            <h2 className="mt-5">Dipartimenti</h2>
-            <br />
-            {dipartimenti.map(d => [
-              <h3>{d.replace(/_/g, ' ')}</h3>,
+        <div className="container">
+          <Switch>
+            <Route exact path="/senato">
+              <h2 className="mt-5">Senato</h2>
+              <br />
+              <Results anno="2018-2020" path="Senato" />
+              <Results anno="2016-2018" path="Senato" />
+            </Route>
+            <Route exact path="/cda">
+              <h2 className="mt-5">Consiglio di Amministrazione</h2>
+              <br />
+              <Results anno="2018-2020" path="Consiglio_di_amministrazione" />
+              <Results anno="2016-2018" path="Consiglio_di_amministrazione" />
+            </Route>
+            <Route exact path="/dipartimenti">
+              <h2 className="mt-5">Dipartimenti</h2>
+              <br />
+              {dipartimenti.map(d => [
+                <Card body className="m-2">
+                  <h3>{d.replace(/_/g, ' ')}</h3>,
               <Results anno="2018-2020" path={`dipartimenti/${d}`} />,
               <Results anno="2016-2018" path={`dipartimenti/${d}`} />
-            ])}
-          </Route>
-          <Route exact path="/not-found">
-            <NotFound />
-          </Route>
-          <Redirect to='/senato' />
-        </Switch>
+                </Card>
+              ])}
+            </Route>
+            <Route exact path="/not-found">
+              <NotFound />
+            </Route>
+            <Redirect to='/senato' />
+          </Switch>
+        </div>
       </HashRouter>
     </div>
   );
--- src/app/results/Results.tsx
+++ src/app/results/Results.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './Results.scss';
 import Collapse from 'react-bootstrap/Collapse';
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
 
 interface Props {
   anno?: string;
@@ -26,69 +30,96 @@ const Results = (props: Props) => {
 
     // generate tableRows
     const tableRows = [];
-    for(let i = 0; i < maxRows; i++)  {
+    for (let i = 0; i < maxRows; i++) {
       tableRows.push(
         <tr key={`${props.anno}-${i}`}>
-          <td></td>
-          {Object.keys(results).map(l => 
+          {Object.keys(results).map(l =>
             <td key={`${props.anno}-${l}-${i}`}>
-
               {(results[l] && results[l][i]) ? (
                 [
                   `${results[l][i].nominativo} (${results[l][i].voti})`,
                   results[l][i].eletto ? (<img key={`coccarda-${i}`} src="coccarda.png" alt="eletto" width="16" height="30" className="float-right" />) : ''
                 ]
               ) : ''}
-
             </td>
           )}
-        </tr>
+       </tr>
       )
     }
 
     return tableRows;
   }
 
+
   function fix_names(name: string): string {
     return name.replace('#', '')
-               .replace(/ /g, ' ')
-               .replace(/ /g, ' ')
-               .replace('ALLENZA UNIVERSITARIA', 'ALLEANZA UNIVERSITARIA')
-               .replace('NIKE  ‐  ARCADIA', 'ARCADIA - NIKE')
-               .replace('INGENERIATTIVA', 'INGEGNERIATTIVA')
-               .replace('LA FINESTRA  ‐  LIBERI DI SCEGLIERE', 'LA FINESTRA ‐ LIBERI DI SCEGLIERE')
-               .replace('LA FINESTRA‐LIBERI DI SCEGLIERE', 'LA FINESTRA ‐ LIBERI DI SCEGLIERE')
-               .replace('LIBERTAS LIBERI E FORTI', 'LIBERTAS')
-               .replace('NUOVA IBLA', 'NUOVAIBLA')
-               .replace('SANI LAB', 'SANILAB')
-               .replace('ECONOMIATTIVA', 'ECONOMIA ATTIVA')
-               .replace('WE LOVE UNICT/CREDIAMOCI', 'WE LOVE UNICT')
-               .replace('WE LOVE UNICT - ARES', 'WE LOVE UNICT')
-               .replace('PARTECIPA ‐ SOS GIURISTI', 'PARTECIPA')
-               .replace('ARCADIA ‐ REVOLUTION', 'ARCADIA REVOLUTION')
-               .replace('UDU - UNIONE DEGLI UNIVERSITARI', 'UDU  ‐  UNIONE DEGLI UNIVERSITARI')
-               .replace(new RegExp("E'", "g"), 'È')
-               .replace(new RegExp("A'", "g"), 'À');
+      .replace(/ /g, ' ')
+      .replace(/ /g, ' ')
+      .replace('ALLENZA UNIVERSITARIA', 'ALLEANZA UNIVERSITARIA')
+      .replace('NIKE  ‐  ARCADIA', 'ARCADIA - NIKE')
+      .replace('INGENERIATTIVA', 'INGEGNERIATTIVA')
+      .replace('LA FINESTRA  ‐  LIBERI DI SCEGLIERE', 'LA FINESTRA ‐ LIBERI DI SCEGLIERE')
+      .replace('LA FINESTRA‐LIBERI DI SCEGLIERE', 'LA FINESTRA ‐ LIBERI DI SCEGLIERE')
+      .replace('LIBERTAS LIBERI E FORTI', 'LIBERTAS')
+      .replace('NUOVA IBLA', 'NUOVAIBLA')
+      .replace('SANI LAB', 'SANILAB')
+      .replace('ECONOMIATTIVA', 'ECONOMIA ATTIVA')
+      .replace('WE LOVE UNICT/CREDIAMOCI', 'WE LOVE UNICT')
+      .replace('WE LOVE UNICT - ARES', 'WE LOVE UNICT')
+      .replace('PARTECIPA ‐ SOS GIURISTI', 'PARTECIPA')
+      .replace('ARCADIA ‐ REVOLUTION', 'ARCADIA REVOLUTION')
+      .replace('UDU - UNIONE DEGLI UNIVERSITARI', 'UDU  ‐  UNIONE DEGLI UNIVERSITARI')
+      .replace(new RegExp("E'", "g"), 'È')
+      .replace(new RegExp("A'", "g"), 'À');
+  }
+
+  function generateNOTA(): JSX.Element {
+    return (
+      <thead>
+        <tr>
+          <th>Schede Bianche</th>
+          <th>Schede Nulle</th>
+          <th>Schede Contestate</th>
+          <th>Votanti</th>
+        </tr>
+        <tr>
+          <td>
+            {data.schede['Schede Bianche']}
+          </td>
+          <td>
+            {data.schede['Schede Nulle']}
+          </td>
+          <td>
+            {data.schede['Schede Contestate']}
+          </td>
+          <td>
+            {data.perc_votanti}
+          </td>
+        </tr>
+      </thead>
+    );
   }
 
   function generateHead(): JSX.Element {
     return (
-        <thead>
+      <thead className="cursorPointer">
         <tr
+          className="head-row"
           onClick={toggleBody}
           aria-controls="example-collapse-text"
-          aria-expanded={show}
-        >
-          <th className="year">{props.anno} </th>
-          { data.liste.map((l: any) =>
-          <th key={props.anno + '-lista-' + l.nome}>
-            <div className="logo" key={props.anno + '-logo-' + l.nome}>
-              <img src={`loghi/${fix_names(l.nome)}.jpg`} width="80" height="80" alt={l.nome}></img>
-            </div>
-            <div className="sub-logo" key={props.anno + '-name-' + l.nome}>
-              {l.nome} ({l.voti_totali})
-            </div>
-          </th>) }
+          aria-expanded={show}>
+          {data.liste.map((l: any) =>
+            <OverlayTrigger placement="top"
+              overlay={tooltipExpandCollapse}>
+              <th key={props.anno + '-lista-' + l.nome}>
+                <div className="logo" key={props.anno + '-logo-' + l.nome}>
+                  <img src={`loghi/${fix_names(l.nome)}.jpg`} width="80" height="80" alt={l.nome}></img>
+                </div>
+                <div className="sub-logo" key={props.anno + '-name-' + l.nome}>
+                  {l.nome} ({l.voti_totali})
+                </div>
+              </th>
+            </OverlayTrigger>)}
         </tr>
       </thead>
     );
@@ -99,41 +130,44 @@ const Results = (props: Props) => {
     setShow(!show);
   }
 
-  useEffect(() => {}, [show]);
+  useEffect(() => { }, [show]);
+
+  const tooltipExpandCollapse = (props: any) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Click to Expand/Collapse
+    </Tooltip>
+  );
 
   return (
     <div className="Results">
-      <div className="container-fluid">
+      <div className="container-fluid p-4">
+        <Card>
+          <Card.Header><b>{props.anno}</b></Card.Header>
+          <Card.Body>
+            <div className="row">
+              <div className="col-12 lists">
+                {/* <h2>Dipartimento: {data.dipartimento}</h2> */}
 
-        <div className="row">
-          <div className="col-12">
-            {/* <h2>Dipartimento: {data.dipartimento}</h2> */}
-
-            <div className={show ? 'invisible' : 'visible'}>
-              <Collapse in={!show}>
-                <Table striped bordered hover responsive className="liste">
+                <Table striped bordered hover className="liste">
                   {generateHead()}
+
+                  <Collapse in={show}>
+                  <div className="generatedRows">
+                  <tbody>
+                    {generateTableRows(data)}
+                  </tbody></div></Collapse>         
                 </Table>
-              </Collapse>
+              </div>
             </div>
-
-              <Collapse in={show}>
-                <div id="example-collapse-text">
-                  <Table striped bordered hover className="liste">
-                    {generateHead()}
-                    <tbody>
-                      {generateTableRows(data)}
-                    </tbody>
-                  </Table>
-                </div>
-              </Collapse>
-
-          </div>
-        </div>
-
-        <br />
-        <br />
-
+            <div className="row">
+              <div className="col-12 mt-2">
+                <Table striped bordered hover responsive className="liste">
+                  {generateNOTA()}
+                </Table>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
