819299cf5e4b05104ef87199b7fc18cafd4c0935
Helias <stefanoborzi32@gmail.com>
2020-10-02 17:16:21 +0200

    chore(contacts): improve code

--- src/app/contacts/Contacts.tsx
+++ src/app/contacts/Contacts.tsx
@@ -5,6 +5,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Contacts.scss';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
+interface ContactDetails {
+  nominativo: string;
+  telegram: string;
+  github: string;
+  email: string;
+  linkedin?: string;
+}
+
+const Contact = (props: ContactDetails): JSX.Element => {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between align-items-center">
+      {props.nominativo}
+      <span className="badge-pill">
+        <Button href={`https://t.me/${props.telegram}`} target="_blank">
+          <div>
+          <FontAwesomeIcon icon={faTelegramPlane} />
+          </div>
+        </Button>{' '}
+
+        <Button href={props.email} target="_blank">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Button>{' '}
+
+        {props?.linkedin && (<Button href={props.linkedin} target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </Button>)}
+        {props?.linkedin && ' '}
+
+        <Button href={`https://github.com/${props.github}`} target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </Button>{' '}
+      </span>
+
+    </ListGroup.Item>
+  );
+};
+
 const Contacts = () => {
 
   return (
@@ -13,127 +50,52 @@ const Contacts = () => {
         <h2 className="mt-5">Contatti</h2>
         <h2 className="mt-5">Questo progetto è stato realizzato da:</h2>
         <ListGroup className="contributors">
-            <ListGroup.Item className="d-flex justify-content-between align-items-center">
-              Stefano Borzì
-              <span className="badge-pill">
-              <Button href="https://t.me/Helias" target="_blank">
-                <div>
-                <FontAwesomeIcon icon={faTelegramPlane} />
-                </div>
-              </Button>{' '}
 
-              <Button href="stefanoborzi32@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </Button>{' '}
+            <Contact
+              nominativo="Stefano Borzì"
+              telegram="Helias"
+              email="stefanoborzi32@gmail.com"
+              linkedin="https://www.linkedin.com/in/stefanoborzi"
+              github="Helias"
+            ></Contact>
 
-              <Button href="https://linkedin.com/in/stefanoborzi/" target="_blank">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </Button>{' '}
+            <Contact
+              nominativo="Vincenzo Filetti"
+              telegram="Veenz"
+              email="vzfiletti@gmail.com"
+              linkedin="https://www.linkedin.com/in/vincenzo-filetti-b87016103/"
+              github="Veeenz"
+            ></Contact>
 
-              <Button href="https://github.com/Helias" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </Button>{' '}
-              </span>
-            
-            </ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-between align-items-center">
-              Vincenzo Filetti
+            <Contact
+              nominativo="Andrea Maugeri"
+              telegram="v0lp3"
+              email="andreamaujeri@gmail.com"
+              github="v0lp3"
+            ></Contact>
 
-              <span className="badge-pill">
-              <Button href="https://t.me/Veenz" target="_blank">
-                <FontAwesomeIcon icon={faTelegramPlane} />
-              </Button>{' '}
+            <Contact
+              nominativo="Graziano Di Grande"
+              telegram="Adrenoid"
+              email="graziano27dg@gmail.com"
+              linkedin="https://www.linkedin.com/in/graziano-di-grande-47b077193/"
+              github="drendog"
+            ></Contact>
 
-              <Button href="vzfiletti@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </Button>{' '}
+            <Contact
+              nominativo="Luigi Seminara"
+              telegram="Gigii_G"
+              email="seminara.luigi@gmail.com"
+              github="Gigi-G"
+            ></Contact>
 
-              <Button href="https://www.linkedin.com/in/vincenzo-filetti-b87016103/" target="_blank">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </Button>{' '}
-
-              <Button href="https://github.com/Veeenz" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </Button>{' '}
-              </span>
-            </ListGroup.Item>
-
-            <ListGroup.Item className="d-flex justify-content-between align-items-center">
-              Andrea Maugeri
-
-              <span className="badge-pill">
-              <Button href="https://t.me/v0lp3" target="_blank">
-                <FontAwesomeIcon icon={faTelegramPlane} />
-              </Button>{' '}
-
-              <Button href="andreamaujeri@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </Button>{' '}
-
-              <Button href="https://github.com/v0lp3" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </Button>{' '}
-              </span>
-            </ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-between align-items-center">
-              Graziano Di Grande
-
-              <span className="badge-pill">
-              <Button href="https://t.me/Adrenoid" target="_blank">
-                <FontAwesomeIcon icon={faTelegramPlane} />
-              </Button>{' '}
-
-              <Button href="graziano27dg@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </Button>{' '}
-
-              <Button href="https://www.linkedin.com/in/graziano-di-grande-47b077193/" target="_blank">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </Button>{' '}
-
-              <Button href="https://github.com/drendog" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </Button>{' '}
-              </span>
-            </ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-between align-items-center">
-              Luigi Seminara
-
-              <span className="badge-pill">
-              <Button href="https://t.me/Gigii_G" target="_blank">
-                <FontAwesomeIcon icon={faTelegramPlane} />
-              </Button>{' '}
-
-              <Button href="seminara.luigi@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </Button>{' '}
-
-              <Button href="https://github.com/Gigi-G" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </Button>{' '}
-              </span>
-            </ListGroup.Item>
-            <ListGroup.Item className="d-flex justify-content-between align-items-center">
-              Davide Carnemolla
-
-              <span className="badge-pill">
-              <Button href="https://t.me/herbrant" target="_blank">
-                <FontAwesomeIcon icon={faTelegramPlane} />
-              </Button>{' '}
-
-              <Button href="cdavide98carnemolla@gmail.com" target="_blank">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </Button>{' '}
-
-              <Button href="https://www.linkedin.com/in/davide-carnemolla" target="_blank">
-                <FontAwesomeIcon icon={faLinkedin} />
-              </Button>{' '}
-
-              <Button href="https://github.com/herbrant" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </Button>{' '}
-              </span>
-            </ListGroup.Item>
+            <Contact
+              nominativo="Davide Carnemolla"
+              telegram="herbrant"
+              email="cdavide98carnemolla@gmail.com"
+              linkedin="https://www.linkedin.com/in/davide-carnemolla"
+              github="herbrant"
+            ></Contact>
         </ListGroup>
       </div>
     </div>
