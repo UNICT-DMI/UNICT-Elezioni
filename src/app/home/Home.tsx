af64e51888cff9aeb59d4f3056c34e2008458b27
drendog <graziano27dg@gmail.com>
2021-04-26 00:20:46 +0200

    feat: add maintenance message

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.scss';
 import Icon from '@mdi/react';
-import { mdiBallotOutline, mdiDatabase, mdiGestureTap, mdiGithub, mdiVote } from '@mdi/js';
+import { mdiBallotOutline, mdiDatabase, mdiGestureTap, mdiGithub, mdiProgressWrench, mdiVote } from '@mdi/js';
 
 function Home(): JSX.Element {
   return (
@@ -20,28 +20,39 @@ function Home(): JSX.Element {
           <a className="btn btn-outline-primary" href="#">Coming soon</a>
         </div>
         <div className="ballot ballot-box text-secondary">
-          <Icon path={mdiVote} size={15}/>
+          <Icon path={mdiVote} size={15} />
         </div>
         <div className="ballot ballot-sheet text-secondary">
-          <Icon path={mdiBallotOutline} size={15}/>
+          <Icon path={mdiBallotOutline} size={15} />
         </div>
       </div>
-      <div className="container">
+      <div className="container py-4">
         <div className="row">
           <div className="col-lg-4">
-            <Icon path={mdiDatabase} size={3}/>
+            <Icon path={mdiDatabase} size={3} />
             <h2>Dati ufficiali</h2>
             <p>Basato su dati estratti da documenti PDF pubblicati sul sito dell&apos;Università di Catania.</p>
           </div>
           <div className="col-lg-4">
-            <Icon path={mdiGithub} size={3}/>
+            <Icon path={mdiGithub} size={3} />
             <h2>Free and Open</h2>
             <p>Progetto completamente Free and Open Source. Chiunque può contribuire per migliorare il progetto.</p>
           </div>
           <div className="col-lg-4">
-            <Icon path={mdiGestureTap} size={3}/>
+            <Icon path={mdiGestureTap} size={3} />
             <h2>Semplice</h2>
             <p>Permette di fornire una visione dei risultati delle elezioni in modo semplice e veloce.</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-7 align-self-center">
+            <h1>Progetto in manutenzione</h1>
+          </div>
+          <div className="col-md-5">
+            <Icon path={mdiProgressWrench} size={10} />
           </div>
         </div>
       </div>
