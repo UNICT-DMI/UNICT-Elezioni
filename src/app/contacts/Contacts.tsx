d01d8bc02cd972df5fdc34ab8aa7c6e05c23bbeb
Helias <stefanoborzi32@gmail.com>
2023-10-29 00:18:40 +0200

    fix: indentation

--- src/app/contacts/Contacts.tsx
+++ src/app/contacts/Contacts.tsx
@@ -32,11 +32,9 @@ const Contact = (props: ContactDetails): JSX.Element => {
           <FontAwesomeIcon icon={faEnvelope} />
         </Button>{' '}
 
-        {props?.linkedin
-        ? (<Button href={props.linkedin} target="_blank">
+        {props?.linkedin ? (<Button href={props.linkedin} target="_blank">
           <FontAwesomeIcon icon={faLinkedin} />
-        </Button>)
-        : null}
+        </Button>) : null}
         {props?.linkedin ? ' ' : null}
 
         <Button href={`https://github.com/${props.github}`} target="_blank">
